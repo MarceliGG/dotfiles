8ef9eb3d32255bffdb79ad49261d069dbf295aca
MarceliGG <marcel.giemza@outlook.com>
2024-03-22 10:29:35 +0100

    uptade

--- .config/emacs/init.el
+++ .config/emacs/init.el
@@ -7,7 +7,8 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(custom-enabled-themes '(wombat))
+ '(custom-safe-themes
+   '("ee1ad72e8e92b34e0d2330896b22eed0a92c88a052ed33253235e8c2fed19884" "c970d7be6a4f7b3583cb8d9574bda23ba31fadc01253d0db87d3a5509ab104ae" default))
  '(package-selected-packages '(which-key)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.

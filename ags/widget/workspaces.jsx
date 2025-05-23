303ce33063c12c20dfcb8c7b1b6bb0556ef39635
MarceliGG <marcel.giemza@outlook.com>
2025-05-23 14:45:43 +0200

    named  workspace for browser, and some keybord options

--- ags/widget/workspaces.jsx
+++ ags/widget/workspaces.jsx
@@ -4,18 +4,15 @@ import { get_icon } from "../util.js";
 
 export default function Workspaces({ orientation }) {
   const hypr = Hyprland.get_default();
-  // {w.map((ws) => (
-  //   <button
-  //     halign={Gtk.Align.Center}
-  //     className={bind(hypr, "focusedWorkspace").as((fw) =>
-  //       ws === fw.id ? "focused" : "",
-  //     )}
-  //     onClicked={() => ws.focus()}
-  //   >
-  //     {ws}
-  //   </button>
-  // ))}
-  // const classNames = Variable({})
+
+  const addStatic = (arr, id) => {
+    const idx = id === 1 ? 0 : arr.findIndex(w => w.id === id - 1)
+    if (idx < 0)
+      return
+    if (arr.find(w => w.id === id) === undefined)
+      arr.splice(idx+1, 0, { "id": id, "name": id, "static": true })
+  }
+
   return (
     <box className="workspaces" orientation={orientation}>
       {bind(hypr, "workspaces").as(workspaces => {
@@ -23,16 +20,12 @@ export default function Workspaces({ orientation }) {
           .filter(ws => !(ws.id >= -99 && ws.id <= -2)) // filter out special workspaces
           .sort((a, b) => a.id - b.id)
 
-        if (filtered.find(w => w.id === 1) === undefined)
-          filtered.splice(0, 0, { "id": 1, "name": 1, "static": true })
-        if (filtered.find(w => w.id === 2) === undefined)
-          filtered.splice(1, 0, { "id": 2, "name": 2, "static": true })
-        if (filtered.find(w => w.id === 3) === undefined)
-          filtered.splice(2, 0, { "id": 3, "name": 3, "static": true })
-        if (filtered.find(w => w.id === 4) === undefined)
-          filtered.splice(3, 0, { "id": 4, "name": 4, "static": true })
-        if (filtered.find(w => w.id === 5) === undefined)
-          filtered.splice(4, 0, { "id": 5, "name": 5, "static": true })
+
+        addStatic(filtered, 1)
+        addStatic(filtered, 2)
+        addStatic(filtered, 3)
+        addStatic(filtered, 4)
+        addStatic(filtered, 5)
 
         return filtered.map((w) => (
           <button
@@ -53,7 +46,7 @@ export default function Workspaces({ orientation }) {
       })}
       {bind(hypr, "focusedClient").as(client => {
         if (client)
-          return <label ellipsize={3} label={bind(client, "title").as(t => t || client.initialTitle || client.class)} css="margin-right: 20px"/>;
+          return <label ellipsize={3} label={bind(client, "title").as(t => t || client.initialTitle || client.class)} css="margin-right: 20px" />;
         else
           return "";
       })}
